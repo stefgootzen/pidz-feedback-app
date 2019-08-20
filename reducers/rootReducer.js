e29f8d19d37deec910a87c97ea0167970a6c9650
Stef Gootzen <stefgootzen@gmail.com>
2019-08-20 14:19:28 +0200

    feat: add onboarding screens

--- reducers/rootReducer.js
+++ reducers/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import formReducer from './formReducer';
+import onboardingReducer from './onboardingReducer';
 
 export default combineReducers({
   form: formReducer,
+  onboarding: onboardingReducer,
 });
